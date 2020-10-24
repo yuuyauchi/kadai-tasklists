ba445d89ea7993255d2bb199e2d2efb339a657dd
Ubuntu <ubuntu@ip-172-31-35-129.ap-northeast-1.compute.internal>
2020-10-24 06:57:01 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('tasks','TasksController');
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
