f60dcadd8176998e8e267af5a23789bcdf516d63
Ubuntu <ubuntu@ip-172-31-35-129.ap-northeast-1.compute.internal>
2020-10-24 13:51:50 +0000

    displayed edit button if authenticated

--- resources/views/microposts/show.blade.php
+++ resources/views/microposts/show.blade.php
@@ -15,9 +15,9 @@
             <td>{{ $micropost->content }}</td>
         </tr>
     </table>
+    @if (Auth::id() == $micropost->user_id)
     {!! link_to_route('microposts.edit', 'このタスクを編集', ['micropost' => $micropost->id], ['class' => 'btn btn-secondary']) !!}
     {{-- タスク削除フォーム --}}
-    @if (Auth::id() == $micropost->user_id)
     {!! Form::model($micropost, ['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
         {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
